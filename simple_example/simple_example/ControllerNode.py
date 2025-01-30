4d0c838108bf5bc133ac69d04dc6b317d3006727
Michal Nedza <MicnedSQ@gmail.com>
2025-01-30 22:16:01 +0000

    Code clean up and added descriptions of functions

--- simple_example/simple_example/ControllerNode.py
+++ simple_example/simple_example/ControllerNode.py
@@ -7,63 +7,76 @@ class ControllerNode(Node):
   def __init__(self):
     super().__init__("controller_node")
     
+    # Utworzenie subskrybcji do customowego topica /centroids
     self.centroids_subscription = self.create_subscription(Centroids, "/centroids", self.steer_callback, 10)
 
+    # Utworzenie publishera na topic /cmd_vel, który steruje pojazdem w Gazebo
     self.cmd_vel_publisher = self.create_publisher(Twist, "/cmd_vel", 10)
 
+    # Inicjalizacja parametrów regulatora PID
     self.kp = 0.0045
     self.ki = 0.0005
     self.kd = 0.0035
 
+    # Inicjalizacja zmiennych potrzebnych do poprawnego działania regulatora PID
     self.previous_error = 0.0
     self.integral = 0.0
-
     self.start_time = self.get_clock().now()
-
     self.previous_angular_z = 0.0
 
+    # Obliczenie środka obrazu
+    self.screen_center = 1920 / 2
 
+    # Inicjalizacja zmiennej przechowującej prędkość pojazdu
+    self.speed = 15.0
+
+  # Callback dla subscribera topicu /centroids
   def steer_callback(self, msg):
+    # Utworzenie obiektu sterującego pojazdem
     output_cmd_vel = Twist()
-    length = msg.length
-    centroids = msg.centroids
 
+    # Zaczytanie centroidów do zmiennych w callbacku
+    centroids = msg.centroids
     centroid_left_x = centroids[0]
     centroid_left_y = centroids[1]
     centroid_right_x = centroids[2]
     centroid_right_y = centroids[3]
 
-    center = 0
+    # Odległość od lini w pixelach w przypadku wykrycia tylko jednej lini
     offset = 500
+    center = 0
 
+    # Sprawdzenie czy wykryte zostały dwie linie czy jedna. W przypadku wykrycia jednej linii,
+    # środek, którego pojazd ma się trzymać jest zdefiniowany jako odległość o "offset" od
+    # jednej z lini. Jeśli są wykryte oba centroidy, to środkiem jest średnia arytmetyczna pomiędzy
+    # dwoma centroidami
     if centroid_left_x == 0 and centroid_left_y == 0:
       center = centroid_right_x - offset
-      # self.get_logger().info('Only right line visible')
     elif centroid_right_x == 1920 and centroid_right_y == 1080:
       center = centroid_left_x + offset
-      # self.get_logger().info('Only left line visible')
     else:
       center = int((centroid_left_x + centroid_right_x) / 2)    
 
-    screen_center = 1920 / 2
+    # Obliczenie błędu do regulatora PID
+    error = center - self.screen_center
 
-    error = center - screen_center
-
+    # Regulator PID
     time_delta = (self.get_clock().now() - self.start_time).nanoseconds / 1e9
-
     self.integral += error
     max_integral = 20000.0
     self.integral = max(min(self.integral, max_integral), -max_integral)
     derivative = error - self.previous_error
     self.previous_error = error
     
+    # Obliczenie prędkości kątowej do sterowania skrętem kół
     angular_z = -(self.kp * error + self.ki * self.integral * time_delta + self.kd * derivative / time_delta)
 
+    # Zapisanie obliczonych wartości do message'u w topicu sterującego pojazdem - /cmd_vel
     output_cmd_vel.angular.z = angular_z
-    self.get_logger().info(f'{self.integral}')
-    output_cmd_vel.linear.x = 15.0
+    output_cmd_vel.linear.x = self.speed
     self.cmd_vel_publisher.publish(output_cmd_vel)
 
+    # Reset czasu
     self.start_time = self.get_clock().now()
 
 def main(args=None):
--- simple_example/simple_example/ProcessImageNode.py
+++ simple_example/simple_example/ProcessImageNode.py
@@ -7,30 +7,181 @@ import numpy as np
 from centroids_msg.msg import Centroids
 from skimage.morphology import skeletonize
 
+
+def prepareYellowMask(img):
+  hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+  lower_yellow = np.array([27, 120, 41])
+  upper_yellow = np.array([30, 255, 55])
+  binary_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+  kernel = np.ones((11, 11), np.uint8)
+  binary_mask = cv2.dilate(binary_mask, kernel, iterations=2)
+  return binary_mask
+
+
+def extractLargeAreas(contours, img):
+  bounding_boxes = []
+  counter = 0 
+  for i, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    if area > 10000:
+      counter += 1
+      x, y, w, h = cv2.boundingRect(contour)
+      bounding_boxes.append([x, y, w, h])
+      cv2.drawContours(img, [contour], -1, (0, 255, 0), cv2.FILLED)
+  return bounding_boxes
+
+
+def skeletonizeImage(img):
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+  binary_bool = binary > 0
+  skeleton = skeletonize(binary_bool).astype(np.uint8) * 255
+  return skeleton
+
+
+def findBranchPoints(bounding_boxes, skeleton):
+  branch_points = []
+  for box in bounding_boxes:
+    x_start, y_start, width, height = box
+    x_end = x_start + width
+    y_end = y_start + height
+
+    roi = skeleton[y_start:y_end, x_start:x_end]
+
+    padded_roi = np.pad(roi, pad_width=1, mode='constant', constant_values=0)
+
+    kernel = np.ones((3, 3), dtype=np.uint16)
+    neighborhood_sum = cv2.filter2D(padded_roi.astype(np.uint16), -1, kernel)[1:-1, 1:-1]
+
+    branch_point_mask = (roi == 255) & (neighborhood_sum > 3 * 255)
+    branch_point_coords = np.argwhere(branch_point_mask)
+
+    branch_point_coords += [y_start, x_start]
+
+    branch_points.extend(branch_point_coords.tolist())
+  return skeleton, branch_points
+
+
+def deleteBranchPoints(branch_points, skeleton, img):
+  for y, x in branch_points:
+    if x > 1920 / 2:
+      cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+
+      if x + 1 < skeleton.shape[1] and skeleton[y, x + 1] == 255:
+          for i in range(x + 1, skeleton.shape[1]):
+              if skeleton[y, i] == 255:
+                  if i == x + 1:
+                    continue
+                  skeleton[y, i] = 0
+              else:
+                  break
+      if x + 1 < skeleton.shape[1] and y - 1 >= 0 and skeleton[y - 1, x + 1] == 255:
+        i = 1
+        while x + i < skeleton.shape[1] and y - i >= 0:
+            if skeleton[y - i, x + i] == 255:
+                skeleton[y - i, x + i] = 0
+            else:
+                break
+            i += 1
+    else:
+      cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+
+      if x - 1 >= 0 and skeleton[y, x - 1] == 255:
+          for i in range(x - 1, -1, -1):
+              if skeleton[y, i] == 255:
+                  if i == x - 1:
+                      continue
+                  skeleton[y, i] = 0
+              else:
+                  break
+
+      if x - 1 >= 0 and y - 1 >= 0 and skeleton[y - 1, x - 1] == 255:
+          i = 1
+          while x - i >= 0 and y - i >= 0:
+              if skeleton[y - i, x - i] == 255:
+                  skeleton[y - i, x - i] = 0
+              else:
+                  break
+              i += 1
+  return skeleton
+
+
+def calculateCentroids(skeleton):
+  centroids_list = []
+  num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(skeleton)
+  areas = stats[:, cv2.CC_STAT_AREA]
+  sorted_indices = np.argsort(areas)[::-1]
+
+  largest_components_mask = np.zeros_like(skeleton)
+
+  for i in sorted_indices[1:num_labels]:
+      component_label = i
+      largest_components_mask = np.zeros_like(skeleton)
+      largest_components_mask[labels == component_label] = 255
+
+      M = cv2.moments(largest_components_mask)
+      if M['m00'] != 0:
+          cx = int(M['m10'] / M['m00'])
+          cy = int(M['m01'] / M['m00'])
+          centroids_list.append((cx * 2, cy * 2))
+  return centroids_list
+
+
+def selectCentroids(centroids_list):
+  centroid_left_x = 0
+  centroid_left_y = 0
+  centroid_right_x = 1920
+  centroid_right_y = 1080
+
+  for cx, cy in centroids_list:
+    if (cx < 1920 / 2 and cx > centroid_left_x):
+      centroid_left_x = cx
+      centroid_left_y = cy
+    if (cx > 1920 / 2 and cx < centroid_right_x):
+      centroid_right_x = cx
+      centroid_right_y = cy
+  return centroid_left_x, centroid_left_y, centroid_right_x, centroid_right_y
+
+
 class ProcessImageNode(Node):
   def __init__(self):
     super().__init__("process_image_node")
 
+    # Utworzenie subskrybcji do topica przechowującego feed z kamery z Gazebo - /front_camera
     self.image_subscription = self.create_subscription(Image, "/front_camera", self.image_callback, 10)
 
+    # Utworzenie publishera na customowy topic /centroids, wysyłający informacje o środkach linii
+    # do Node'a sterującego pojazdem
     self.publisher = self.create_publisher(Centroids, "centroids", 10)
 
+    # Konwerter z ROS2 do OpenCV
     self.bridge = CvBridge()
 
+    # Bufor na ramkę z kamery
     self.frame = None
 
+  # Główna funkcja wykonująca operacje na obrazie. Callback topicu /front_camera pobierający 
+  # obraz z feedu z kamery. Obraz jest następnie przekształcany w taki sposób, aby był to widok
+  # "z lotu ptaka" (self.perspectiveWarp()). Następnie na obrazie wykrywane są linie oraz 
+  # obliczane są środki linii (self.processImage(resized_frame)).
   def image_callback(self, msg):
     try:
+      # Pobranie pojedynczej ramki z message'u
       self.frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
 
+      # Przekształcenie obrazu na obraz "z lotu ptaka"
       birdseye = self.perspectiveWarp()
+
+      # Zmniejszenie dwukrotne rozdzielczości obrazu w celu optymalizacji
       resized_frame = cv2.resize(birdseye, (960, 540))
+
+      # Wykrywanie linii, obliczanie centroidów linii oraz wysyłanie ich na topic /centroids
       self.processImage(resized_frame)
 
-      self.display_image()
     except Exception as e:
       self.get_logger().info('Error processing image: %s' % str(e))
 
+  # Callback wysyłający message na topic /centroids
   def send_msg(self, centroids_list):
     msg = Centroids()
     msg.length = len(centroids_list)
@@ -40,20 +191,16 @@ class ProcessImageNode(Node):
     msg.centroids = flattened_centroids
     self.publisher.publish(msg)
 
-  def display_image(self):
-    # cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
-    # cv2.imshow("Frame", self.frame)
-    # cv2.imwrite("src/test1.png", self.frame)
-    cv2.waitKey(1)
-
   def perspectiveWarp(self):
+    # Obszar z obrazu wejściowego, który ma być rzutowany
     src = np.float32([
         [520, 600], 
         [1300, 600],
         [0, 840], 
         [1920, 840]
         ])
-
+    
+    # Obszar, na który src ma być rzutowany
     dst = np.float32([
         [0, 0],
         [1920, 0],
@@ -61,184 +208,65 @@ class ProcessImageNode(Node):
         [1920, 1080]
         ])
 
+    # Wyciągnięcie rozmiaru obrazu
     img_size = (self.frame.shape[1], self.frame.shape[0])
 
+    # Rzutowanie obszaru z src na obszar dst
     matrix = cv2.getPerspectiveTransform(src, dst)
     birdseye = cv2.warpPerspective(self.frame, matrix, img_size)
-
-    # cv2.namedWindow("Birdseye", cv2.WINDOW_NORMAL)
-    # cv2.imshow("Birdseye", birdseye)
 
     return birdseye
 
   def processImage(self, birdseye):
-    hsv = cv2.cvtColor(birdseye, cv2.COLOR_BGR2HSV)
-    lower_yellow = np.array([27, 120, 41])
-    upper_yellow = np.array([30, 255, 55])
-    binary_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-    
+    # Przygotowanie maski w kolorze lini wyznaczającej tor jazdy
+    binary_mask = prepareYellowMask(birdseye)
 
-    kernel = np.ones((11, 11), np.uint8)
-    binary_mask = cv2.dilate(binary_mask, kernel, iterations=2)
+    # Znalezienie konturów wszystkich linii
     contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Przekształcenie z obrazu binarnego na obraz o formacie RGB (ale dalej binarny)
     out_img = np.dstack((binary_mask, binary_mask, binary_mask)) * 255
 
-    centroids_list = []
-    bounding_boxes = []
+    # Wyodrębnienie z obrazu tylko dużych obszarów w celu usunięcia zakłóceń
+    bounding_boxes = extractLargeAreas(contours, out_img)
+    
+    # Szkieletyzacja obrazu
+    skeleton = skeletonizeImage(out_img)
 
-    counter = 0 
-    for i, contour in enumerate(contours):
-      area = cv2.contourArea(contour)
-      if area > 10000:
-        counter += 1
-        # M = cv2.moments(contour)
+    # Znajdowanie branch pointów - punktów, w których pojawia się rozwidlenie linii
+    skeleton, branch_points = findBranchPoints(bounding_boxes, skeleton)
 
-        x, y, w, h = cv2.boundingRect(contour)
+    # Usunięcie punktów łączących rozgałęzienia
+    skeleton = deleteBranchPoints(branch_points, skeleton, out_img)
 
-        bounding_boxes.append([x, y, w, h])
+    # Obliczenie centroidów dla każdego z obszarów
+    centroids_list = calculateCentroids(skeleton)
 
-        # if M['m00'] != 0:
-        #   cx = int(M['m10'] / M['m00'])
-        #   cy = int(M['m01'] / M['m00'])
-        #   centroids_list.append([cx, cy])
-        # else:
-        #   cx, cy = 0, 0
-        # cv2.circle(out_img, (cx, cy), 5, (255, 0, 0), -1)
-        cv2.drawContours(out_img, [contour], -1, (0, 255, 0), cv2.FILLED)
+    # Wyszukanie dwóch centroidów na lewo i na prawo od środka obrazu, które są najbliżej środka
+    (centroid_left_x, centroid_left_y,
+      centroid_right_x, centroid_right_y) = selectCentroids(centroids_list)
 
-    gray = cv2.cvtColor(out_img, cv2.COLOR_BGR2GRAY)
-    _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
-
-    binary_bool = binary > 0
-    skeleton = skeletonize(binary_bool).astype(np.uint8) * 255
-
-    branch_points = []
-
-    for box in bounding_boxes:
-      x_start, y_start, width, height = box
-      x_end = x_start + width
-      y_end = y_start + height
-
-      roi = skeleton[y_start:y_end, x_start:x_end]
-
-      padded_roi = np.pad(roi, pad_width=1, mode='constant', constant_values=0)
-
-      kernel = np.ones((3, 3), dtype=np.uint16)
-      neighborhood_sum = cv2.filter2D(padded_roi.astype(np.uint16), -1, kernel)[1:-1, 1:-1]
-
-      branch_point_mask = (roi == 255) & (neighborhood_sum > 3 * 255)
-
-      # cv2.imshow("Neighborhood Sum", (branch_point_mask * 255).astype(np.uint8))
-
-      branch_point_coords = np.argwhere(branch_point_mask)
-
-      branch_point_coords += [y_start, x_start]
-
-      branch_points.extend(branch_point_coords.tolist())
-
-    for y, x in branch_points:
-        if x > 1920 / 2:
-          cv2.circle(out_img, (x, y), 5, (0, 0, 255), -1)
-
-          if x + 1 < skeleton.shape[1] and skeleton[y, x + 1] == 255:
-              for i in range(x + 1, skeleton.shape[1]):
-                  if skeleton[y, i] == 255:
-                      if i == x + 1:
-                        continue
-                      skeleton[y, i] = 0
-                  else:
-                      break
-          if x + 1 < skeleton.shape[1] and y - 1 >= 0 and skeleton[y - 1, x + 1] == 255:
-            i = 1
-            while x + i < skeleton.shape[1] and y - i >= 0:
-                if skeleton[y - i, x + i] == 255:
-                    skeleton[y - i, x + i] = 0
-                else:
-                    break
-                i += 1
-        else:
-          cv2.circle(out_img, (x, y), 5, (0, 0, 255), -1)
-
-          if x - 1 >= 0 and skeleton[y, x - 1] == 255:
-              for i in range(x - 1, -1, -1):
-                  if skeleton[y, i] == 255:
-                      if i == x - 1:
-                          continue
-                      skeleton[y, i] = 0
-                  else:
-                      break
-
-          if x - 1 >= 0 and y - 1 >= 0 and skeleton[y - 1, x - 1] == 255:
-              i = 1
-              while x - i >= 0 and y - i >= 0:
-                  if skeleton[y - i, x - i] == 255:
-                      skeleton[y - i, x - i] = 0
-                  else:
-                      break
-                  i += 1
-           
-    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(skeleton)
-    self.get_logger().info('NO labels: %s' % str(num_labels))
-    areas = stats[:, cv2.CC_STAT_AREA]
-    sorted_indices = np.argsort(areas)[::-1]
-
-    largest_components_mask = np.zeros_like(skeleton)
-
-    for i in sorted_indices[1:num_labels]:
-        component_label = i
-        largest_components_mask = np.zeros_like(skeleton)
-        largest_components_mask[labels == component_label] = 255
-
-        M = cv2.moments(largest_components_mask)
-        if M['m00'] != 0:
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-            centroids_list.append((cx * 2, cy * 2))
-
-    self.get_logger().info('NO centroids: %s' % str(len(centroids_list)))
-
-    centroid_left_x = 0
-    centroid_left_y = 0
-    centroid_right_x = 1920
-    centroid_right_y = 1080
-
-    for cx, cy in centroids_list:
-      if (cx < 1920 / 2 and cx > centroid_left_x):
-        centroid_left_x = cx
-        centroid_left_y = cy
-      if (cx > 1920 / 2 and cx < centroid_right_x):
-        centroid_right_x = cx
-        centroid_right_y = cy
-
-    middle_centroid = int((centroid_left_x + centroid_right_x) / 2)
-
-    # Center line
+    # Dodanie do obrazu wynikowego lini będącej środkiem obrazu
     cv2.line(out_img, (int(1920 / 4), 0), (int(1920 / 4), 1080), (0, 255, 0), 10)
 
-    # Middle centroid
+    # Obliczenie centroidu środkowego
+    middle_centroid = int((centroid_left_x + centroid_right_x) / 2)
+
+    # Dodanie do obrazu wynikowego centroidu środkowego
     cv2.circle(out_img, (int(middle_centroid / 2), int(1080 / 4)), 10, (255, 0, 0), -1)
 
-    cv2.circle(out_img, (int(centroid_left_x / 2), int(centroid_left_y / 2)), 10, (255, 0, 0), -1)
+    # Dodanie do obrazu wynikowego centroidu lewego
+    cv2.circle(out_img, (int(centroid_left_x / 2),int(centroid_left_y / 2)), 10, (255, 0, 0), -1)
+    # Dodanie do obrazu wynikowego centroidu prawego
     cv2.circle(out_img, (int(centroid_right_x / 2), int(centroid_right_y / 2)), 10, (0, 0, 255), -1)
-    
-    # One centorid sent
-    # centroids_to_send = [(middle_centroid, int(1080 / 2))]
 
-    # Two centroids sent
+    # Zapisanie do tablicy centroidów, do wysłania na customowym topicu /centroids
     centroids_to_send = [(centroid_left_x, centroid_left_y), (centroid_right_x, centroid_right_y)]
 
-    # skeleton_with_largest_components = cv2.bitwise_and(skeleton, largest_components_mask)
-    # labels_colored = cv2.applyColorMap(skeleton_with_largest_components.astype(np.uint8), cv2.COLORMAP_JET)
-    # cv2.namedWindow("Largest Connected Components", cv2.WINDOW_NORMAL)
-    # cv2.imshow("Largest Connected Components", labels_colored)
-    # cv2.waitKey(1)
-
-    # cv2.namedWindow("Skeletonized Image", cv2.WINDOW_NORMAL)
-    # cv2.imshow("Skeletonized Image", skeleton)
-    # cv2.waitKey(1)
+    # Wysłanie customowego topica /centroids
     self.send_msg(centroids_to_send)
 
+    # Wyświeltenie obrazu wynikowego
     cv2.namedWindow("Birdseye out_img", cv2.WINDOW_NORMAL)
     cv2.imshow("Birdseye out_img", out_img)
     cv2.waitKey(1)
